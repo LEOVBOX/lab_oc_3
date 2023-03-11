b36abb8dc62fa7d075f3747e0dc07048ba091991
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2023-03-11 18:19:49 +0700

    Split string operation in extra file

---
+++ string_ops.h
@@ -0,0 +1,11 @@
+//
+// Created by Леонид Шайхутдинов on 11.03.2023.
+//
+
+#ifndef LAB_OC_3_STRING_OPS_H
+#define LAB_OC_3_STRING_OPS_H
+
+char* string_reverse(const char* string);
+char* string_cut(const char* string, const unsigned long* begin_ind, const unsigned long* end_ind);
+
+#endif //LAB_OC_3_STRING_OPS_H
