942d8f68a00cfe37adf7f785d6cc60ce1f3c4d8a
LEOVBOX <75016292+LEOVBOX@users.noreply.github.com>
2023-03-12 19:36:32 +0700

    implemented copy_file_reverse

--- copy_file.c
+++ copy_file.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <mm_malloc.h>
 #include <fcntl.h>
+#include "string_ops.h"
 
 #define MAX_BUF_SIZE 1024
 
@@ -37,11 +38,30 @@ int copy_file_reverse(char* from_file_path, char* to_file_path)
 		int res = creat(to_file_path, 0666);
 		to_file = fopen(to_file_path, "w");
 	}
+
 	for (int i = 0; i < inter_count; i++)
 	{
 		fgets(buf, buf_size+1, from_file);
+		string_reverse(buf);
 		fprintf(to_file, "%s", buf);
 	}
+	if (residual_chars > 0)
+	{
+		buf = realloc(buf, residual_chars);
+		if (buf == NULL)
+		{
+			printf("Realloc error\n");
+			exit(1);
+		}
+
+		for (int i = 0; i < residual_chars; i++)
+		{
+			fgets(buf, buf_size+1, from_file);
+			string_reverse(buf);
+			fprintf(to_file, "%s", buf);
+		}
+	}
+	free(buf);
 	fclose(from_file);
 	fclose(to_file);
 	return 0;
